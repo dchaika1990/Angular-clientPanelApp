74bb62f36d01febd9ca4eb90c0a13732528fe144
dchaika1990 <dchaika1990@gmail.com>
2018-02-08 23:32:04 +0200

    Выполнил client-details.component, вывод данные слиентов по id, также сделал кнопку delete с alert

---
+++ src/app/components/add-client/add-client.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FlashMessagesService } from "angular2-flash-messages";
+
+import { Client } from "../../models/client";
+import { ClientService } from "../../services/client.service";
+
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-add-client',
+  templateUrl: './add-client.component.html',
+  styleUrls: ['./add-client.component.css']
+})
+export class AddClientComponent implements OnInit {
+
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0
+  };
+
+  disabledBalanceOnAdd: boolean = true;
+
+  @ViewChild('clientForm') form: any;
+
+  constructor(
+    private clientService: ClientService,
+    private flashMessages: FlashMessagesService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    console.log(this.form);
+
+  }
+
+  onSubmit() {
+
+    if ( !this.form.valid ) {
+      this.flashMessages.show('Please enter form', {
+        timeout: 4000,
+        cssClass: 'alert-danger'
+      })
+    } else {
+      // Add new client
+      this.clientService.newClient(this.client);
+      // show message success
+      this.flashMessages.show('New client add success', {
+        cssClass: 'alert-success',
+        timeout: 4000
+      });
+      // Redirect
+      this.router.navigate(['/']);
+    }
+
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBFmWVoL1HSQdvZWILHaNrdQQn2D2isN1I",
+    authDomain: "clientpanelapp-dbb61.firebaseapp.com",
+    databaseURL: "https://clientpanelapp-dbb61.firebaseio.com",
+    projectId: "clientpanelapp-dbb61",
+    storageBucket: "clientpanelapp-dbb61.appspot.com",
+    messagingSenderId: "146312446863"
+  }
+};
